300a6096e6bd0b723c0bc704b0929a409180320f
harshkumare <160692849+harshkumare@users.noreply.github.com>
2024-02-21 15:57:12 +0530

    Update 1project.c

--- 1project.c
+++ 1project.c
@@ -5,10 +5,10 @@ int main ()
 { int number, guess, nguesses =1;
 srand(time (0));
 number = rand()%100+1;
-do { printf( "enter your number" );
+do { printf( "enter your number \n" );
     scanf(" %d", & guess );
-    if (number > guess){ printf("higher number plzz");}
-    else if(number<guess){printf("lower number plzz");}
+    if (number > guess){ printf("higher number plzz \n");}
+    else if(number<guess){printf("lower number plzz \n");}
     else{printf(" you guessed it right on %d attempts", nguesses);}
     nguesses++;}while (number!= guess);
     return 0;
